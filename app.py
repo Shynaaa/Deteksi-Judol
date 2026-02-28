304f52fcb2cae003066f251aac0defc891bae4d4
Shynaaa <113772681+Shynaaa@users.noreply.github.com>
2026-02-28 14:00:19 +0700

    app.py

--- app.py
+++ app.py
@@ -411,4 +411,6 @@ def upload():
 # ================= RUN =================
 
 if __name__ == "__main__":
-    app.run()
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
